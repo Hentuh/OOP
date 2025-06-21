1a6956e23f505d6fddb02e21ee87e3cb945d954c
Hentuh <amusicant24@gmail.com>
2025-06-21 14:00:41 +0300

    lab1

--- lab1/obj/Debug/net9.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net9.0/lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4acfa20acb4a617d02b28721060fa75b7bb22c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
